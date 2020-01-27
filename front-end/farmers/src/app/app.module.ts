75f160ba4b207024871cfe42a8ed00a3cd897001
Anderson Martins <anderolimar@gmail.com>
2020-01-27 01:10:13 -0300

    Add FormModule

--- front-end/farmers/src/app/app.module.ts
+++ front-end/farmers/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { MatInputModule } from '@angular/material/input';
     MatCardModule,
     MatFormFieldModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- front-end/farmers/src/app/farmer-search-card/farmer-search-card.component.ts
+++ front-end/farmers/src/app/farmer-search-card/farmer-search-card.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FarmerSearchCardComponent implements OnInit {
 
-  value = '';
+  value: string;
 
   constructor() {
 
