8aa29c9d3bdcb1b8058824dd14b31fe330ae3dfc
Anderson Martins <anderolimar@gmail.com>
2020-01-31 12:31:01 +0000

    CHECKPOINT

--- back-end/farmers/app/models/data/farmer.js
+++ back-end/farmers/app/models/data/farmer.js
@@ -5,8 +5,7 @@ const Address = require('./address')
 class Farmer {
   constructor(args) {
       const currArgs = args || {}; 
-      this._id = currArgs._id || null;
-      this.id = this._id;
+      this.id = currArgs._id || null;
       this.name =  currArgs.name;
       this.document = new Document(currArgs.document);
       this.address = new Address(currArgs.address);
@@ -14,6 +13,7 @@ class Farmer {
   
   toObject() {
       const obj = Object.assign({}, this)
+      obj._id = this.id
       obj.document = Object.assign({}, obj.document )
       obj.address = Object.assign({}, obj.address )
       return obj;
